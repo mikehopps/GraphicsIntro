0556a943eff2b0d573a09078d17616207af86293
mikehopps <mhopps@gmail.com>
2017-09-25 11:21:31 -0400

    Initial commit

---
+++ src/GraphicsPanel.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+import java.awt.*;
+
+
+
+public class GraphicsPanel extends JPanel{
+
+    public GraphicsPanel(int width, int height){
+        setSize(width,height);
+    }
+    public void paintComponent(Graphics g){
+        Graphics2D g2 = (Graphics2D)g;
+
+    }
+
+
+
+
+}
