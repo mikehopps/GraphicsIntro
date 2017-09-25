f158bab28c74be1f3099b4ff3741af139bf56401
mikehopps <mhopps@gmail.com>
2017-09-25 14:23:11 -0400

    Graphics Day 1 - basic g2.draw____ Stroke Font Color

--- src/GraphicsPanel.java
+++ src/GraphicsPanel.java
@@ -10,6 +10,23 @@ public class GraphicsPanel extends JPanel{
     public void paintComponent(Graphics g){
         Graphics2D g2 = (Graphics2D)g;
 
+        g2.setColor(new Color(54, 40, 177));
+        g2.fillOval(0, 0, getWidth(), getHeight());
+
+        g2.setColor(new Color(238, 140, 12));
+        g2.fillRect(getWidth()/2-100,getHeight()/2-150, 200, 300);
+
+        g2.setStroke(new BasicStroke(10));
+
+        int[] xs = {getWidth()/2, getWidth(), 0};
+        int[] ys = {0, getHeight(), getHeight()};
+        g2.drawPolygon(xs, ys, 3);
+
+        g2.setColor(Color.BLACK);
+
+        g2.setFont(new Font("Chalkduster", Font.BOLD, 40));
+        g2.drawString("GRAFICKS!", 300, 400);
+
     }
 
 }
